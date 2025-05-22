75fb1f4fb2ebee4b45bb4f5c8480c4f9a52141be
princeraval147 <princeraval147@gmail.com>
2025-05-22 17:44:18 +0530

    Add Routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home';
 import Login from './components/User/Login';
 import ViewStock from './components/Stocks/ViewStock';
 import ReadCSV from './components/ReadCSV/ReadCSV';
+import StockData from './components/User/StockData';
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
@@ -14,6 +15,7 @@ const Router = createBrowserRouter(
             <Route index element={<Home />} />
             <Route path='login' element={<Login />} />
             <Route path='view-stock' element={<ViewStock />} />
+            <Route path='stock-data' element={<StockData />} />
             <Route path='read-excel' element={<ReadExcleData />} />
             <Route path='read-csv' element={<ReadCSV />} />
             <Route path='*' element={<Error />} />
