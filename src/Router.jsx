a6f34c57af5f10d73411d7ed4feb64b22b4e1890
princeraval147 <princeraval147@gmail.com>
2025-05-27 15:22:30 +0530

    Manage Routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,20 +5,21 @@ import Error from './components/Error';
 import ReadExcleData from './components/ReadExcle/ReadExcleData';
 import Home from './components/Home/Home';
 import Login from './components/User/Login';
-import ViewStock from './components/Stocks/ViewStock';
 import ReadCSV from './components/ReadCSV/ReadCSV';
-import StockData from './components/User/StockData';
+import StockData from './components/Stock Data/StockData';
+// import ProtectedRoute from './ProtectedRoute';
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App />}>
             <Route index element={<Home />} />
             <Route path='login' element={<Login />} />
-            <Route path='view-stock' element={<ViewStock />} />
-            <Route path='stock-data' element={<StockData />} />
             <Route path='read-excel' element={<ReadExcleData />} />
             <Route path='read-csv' element={<ReadCSV />} />
             <Route path='*' element={<Error />} />
+            {/* <Route element={<ProtectedRoute />}> */}
+            <Route path='stock-data' element={<StockData />} />
+            {/* </Route> */}
         </Route>
     )
 )
