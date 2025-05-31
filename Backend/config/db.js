ebcd2af23469912c96320778e137cf038f69d71d
princeraval147 <princeraval147@gmail.com>
2025-05-31 12:55:13 +0530

    Bug Fix

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,7 +6,10 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    multipleStatements: true
+    multipleStatements: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 })
 
 module.exports = db;
