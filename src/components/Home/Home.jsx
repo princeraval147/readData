5a80e6760c40470c69672b3ede53d9bb9f010423
princeraval147 <princeraval147@gmail.com>
2025-06-11 16:02:46 +0530

    Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomePage from './HomePage'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <HomePage />
         </div>
     )
 }
