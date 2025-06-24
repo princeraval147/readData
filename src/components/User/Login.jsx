e296194bc1c73d4f48c81c3362a7c7aa7bd8a605
princeraval147 <princeraval147@gmail.com>
2025-06-24 18:49:27 +0530

    Login Page

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,75 +1,23 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import API from '../../API'
 import { Alert, Snackbar } from "@mui/material";
+import { FaGem } from "react-icons/fa";
 
 const Login = () => {
-    const [activeTab, setActiveTab] = useState("signup");
-    const [resetStep, setResetStep] = useState(0); // 0 -> Normal Login, 1 -> Enter Email, 2 -> Enter OTP & New Password
+
     const Navigate = useNavigate();
-
-    // Signup & Login State
-    const [signupData, setSignupData] = useState({
-        username: "",
-        email: "",
-        password: "",
-    });
-
     const [loginData, setLoginData] = useState({
         email: "",
         password: "",
     });
 
-    const [resetData, setResetData] = useState({
-        email: "",
-        password: "",
-    });
-
-    // Handle input change
-    // const handleChange = (e, setState) => {
-    //     setState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    // };
-
     const handleChange = (e, setState) => {
         const { name, value } = e.target;
         const newValue = name === "email" ? value.toLowerCase() : value;
         setState((prev) => ({ ...prev, [name]: newValue }));
-    };
-
-
-
-    // Handle Signup Submission
-    const handleSignupSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await API.post("/auth/register", signupData);
-            const data = response.data;
-            alert("Registration successful!");
-            setResetStep(0);
-            setActiveTab("login");
-            setSignupData({ username: "", email: "", password: "" });
-            // localStorage.setItem("token", data.token);
-            // document.cookie = `token=${data.token};`;
-        } catch (error) {
-            if (error.response && error.response.status === 409) {
-                alert(error.response.data.message); // "Email already exists"
-                setSignupData({ username: "", email: "", password: "" });
-            } else {
-                alert("Something went wrong. Please try again.");
-                setSignupData({ username: "", email: "", password: "" });
-                console.error("Error during signup:", error);
-            }
-        }
-    };
-
-    const [open, setOpen] = useState(false);
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpen(false);
     };
 
     const handleLoginSubmit = async (e) => {
@@ -82,42 +30,23 @@ const Login = () => {
                 }
             );
             const data = response.data;
-            // Set cookie(24h)
-            // document.cookie = `token=${data.token}; path=/; max-age=86400; SameSite=Strict;`;
-
-            // also store in localStorage
-            // localStorage.setItem("token", data.token);
 
             window.dispatchEvent(new Event('user-logged-in')); // Tell Header to recheck
 
             alert(data.message);
             Navigate("/stock-data");
-            // Navigate("/");
 
         } catch (error) {
             if (error.response && error.response.data && error.response.data.message) {
-                setOpen(true); // This shows the snackbar
                 alert(error.response.data.message);
             } else {
-                setOpen(true); // This shows the snackbar
                 alert("Something went wrong. Please try again.");
                 console.error("Error while Login : ", error);
             }
         }
     };
 
-    // Handle Forgot Password Steps
-    const handleForgotPassword = async (e) => {
-        e.preventDefault();
 
-        const response = await API.post("/auth/forgot-password", {
-            email: e.target.email.value,
-            password: e.target.password.value,
-        });
-        setResetStep(0);
-        const data = await response.data;
-        alert(data.message);
-    };
 
 
 
@@ -126,121 +55,24 @@ const Login = () => {
 
 
     return (
-        <div className={styles.logincontainer}>
+        <>
+            <div className="h-130 bg-gray-100 flex items-center justify-center px-4">
+                <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
+                    <div className="flex justify-center mb-4">
+                        <FaGem className="text-indigo-500 text-3xl" />
+                    </div>
+                    <h2 className="text-xl text-center font-semibold text-gray-700 mb-6">
+                        Login to Platinum Diam
+                    </h2>
 
-            <Snackbar
-                open={open}
-                autoHideDuration={2000}
-                onClose={handleClose}
-                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            >
-                <Alert
-                    onClose={handleClose}
-                    severity="error"
-                    variant="filled"
-                    sx={{ width: '100%' }}
-                >
-                    Login Failed!
-                </Alert>
-            </Snackbar>
-
-            <div className={styles.formBox}>
-                {/* Tabs */}
-                <div className={styles.tabs}>
-                    <button
-                        className={activeTab === "signup" ? styles.active : ""}
-                        onClick={() => {
-                            setActiveTab("signup");
-                            setResetStep(0);
-                        }}
-                    >
-                        Signup
-                    </button>
-                    <button
-                        className={activeTab === "login" ? styles.active : ""}
-                        onClick={() => {
-                            setActiveTab("login");
-                            setResetStep(0);
-                        }}
-                    >
-                        Login
-                    </button>
-                </div>
-
-                {/* Signup Form */}
-                {activeTab === "signup" && (
-                    <form onSubmit={handleSignupSubmit} className={styles.form}>
-                        <h2>Create Account</h2>
-                        {/* <p>Create an account to get started.</p> */}
-
-                        <input
-                            type="text"
-                            name="username"
-                            placeholder="Username"
-                            value={signupData.username}
-                            onChange={(e) => handleChange(e, setSignupData)}
-                            className={styles.inputText}
-                            required
-                        />
+                    <form onSubmit={handleLoginSubmit} className="space-y-4">
                         <input
                             type="email"
                             name="email"
-                            placeholder="Email"
-                            value={signupData.email}
-                            onChange={(e) => handleChange(e, setSignupData)}
-                            className={styles.inputText}
-                            required
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            value={signupData.password}
-                            onChange={(e) => handleChange(e, setSignupData)}
-                            className={styles.inputText}
-                            required
-                        />
-                        <button type="submit">Register</button>
-                    </form>
-                )}
-
-                {/* Forgot Password - Enter Email */}
-                {resetStep === 1 && (
-                    <form onSubmit={handleForgotPassword} className={styles.form}>
-                        <h2>Forgot Password</h2>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            value={resetData.email}
-                            onChange={(e) => handleChange(e, setResetData)}
-                            className={styles.inputText}
-                            required
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="password"
-                            value={resetData.password}
-                            onChange={(e) => handleChange(e, setResetData)}
-                            className={styles.inputText}
-                            required
-                        />
-                        <button type="submit">Reset Password</button>
-                    </form>
-                )}
-
-                {/* Login */}
-                {activeTab === "login" && resetStep === 0 && (
-                    <form onSubmit={handleLoginSubmit} className={styles.form}>
-                        <h2>Login</h2>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
+                            placeholder="Email Address"
                             value={loginData.email}
                             onChange={(e) => handleChange(e, setLoginData)}
-                            className={styles.inputText}
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-400 outline-none"
                             required
                         />
                         <input
@@ -249,26 +81,33 @@ const Login = () => {
                             placeholder="Password"
                             value={loginData.password}
                             onChange={(e) => handleChange(e, setLoginData)}
-                            className={styles.inputText}
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-400 outline-none"
                             required
                         />
-                        <p
-                            className="link"
-                            style={{ cursor: "pointer" }}
-                            onClick={() => {
-                                setActiveTab("signup");
-                                setResetStep(0);
-                            }}
+                        {/* {error && <p className="text-red-500 text-sm">{error}</p>} */}
+                        <p className="text-sm text-center text-gray-500">
+                            <NavLink to="/forget-password" className="text-indigo-600 font-medium hover:underline">
+                                Forgot Password
+                            </NavLink>
+                        </p>
+
+                        <button
+                            type="submit"
+                            className="w-full bg-indigo-500 hover:bg-indigo-600 text-white py-2 rounded-md transition"
                         >
-                            Don't have Account</p>
-                        <button type="submit">Login</button>
-                        <button type="button" className={styles.forgotPassword} onClick={() => setResetStep(1)}>
-                            Forgot Password?
+                            Login
                         </button>
                     </form>
-                )}
+
+                    <p className="text-sm text-center mt-6 text-gray-500">
+                        Don’t have an account?{' '}
+                        <NavLink to="/register" className="text-indigo-600 font-medium hover:underline">
+                            Register
+                        </NavLink>
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
